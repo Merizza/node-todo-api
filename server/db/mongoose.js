b65bad2182c643998128c84dc7cb8da275c10274
Merizza Khan <merizza.azwir@gmail.com>
2017-10-05 10:48:49 +0100

    Setting mlab connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	
