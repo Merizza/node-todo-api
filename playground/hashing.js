4f8649b80d96c5e21103ba92e7cf6f3e2f91b36e
Merizza Khan <merizza.azwir@gmail.com>
2017-10-11 21:26:15 +0100

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, 'somesecret');
+console.log(token);
+
+var decoded = jwt.verify(token, 'somesecret');
+console.log('Decoded', decoded);
+
+//var message = 'I am user number 1';
+//var hash = SHA256(message).toString();
+//
+//console.log(`Message: ${message}`);
+//console.log(`Hash: ${hash}`);
+//
+//var data = {
+//	id: 4
+//};
+//
+//var token = {
+//	data,
+//	hash: SHA256(JSON.stringify(data) + 'secret').toString()
+//};
+//
+//token.data.id = 5;
+//token.hash = SHA256(token.data).toString();
+//
+//var resultHash = SHA256(JSON.stringify(token.data) + 'secret').toString();
+//
+//if (resultHash === token.hash) {
+//	console.log('Data was not changed');
+//} else {
+//	console.log('Data has changed. Do not trust!');
+//}
--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,148 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+/*
+Setting up the user model
 
-var User = mongoose.model('Users', {
+{
+	email: 'momo@example.com',
+	password: 'dbfsskskfnsddbdsfsk',
+	tokens: [{
+		acces: 'auth',
+		token: 'sdbfskfbkjfsjkbkjhwkk'
+	}]
+
+}
+
+*/
+
+var UserSchema = new mongoose.Schema({
 	
 	email: {
 		type: String,
 		required: true,
 		minLength: 1,
-		trim: true
-	}
+		trim: true,
+		unique: true,	//prevent a duplicated email
+		validate: {
+//			validator: (value) => {
+//				return validator.isEmail(value);
+//			},
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email',
+			required: [true, 'User email required']
+		}
+	}, 
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		}
+	}, {
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 	
 });
 
-module.exports = {
-	User: User
+UserSchema.methods.toJSON = function() {
+	
+	var user = this;
+	var userObject = user.toObject();
+	
+	return _.pick(userObject, ['_id', 'email']);
+	
+}
+
+UserSchema.methods.generateAuthToken = function () {
+	
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+	//var token = jwt.sign({_id: user._id.toHexString(), access: access}, 'abc123').toString();
+	user.tokens.push({access, token});
+	//user.tokens.push({access: access, token: token});
+	
+	return user.save().then(() => {
+		return token;
+	});
+	
+//	user.save().then(() => {
+//		return token;
+//	}).then((token) => {
+//		
+//	});
+	
 };
+
+var User = mongoose.model('Users', UserSchema);
+
+module.exports = {User};
+
+
+
+//var User = mongoose.model('Users', {
+//	
+//	email: {
+//		type: String,
+//		required: true,
+//		minLength: 1,
+//		trim: true,
+//		unique: true,	//prevent a duplicated email
+//		validate: {
+////			validator: (value) => {
+////				return validator.isEmail(value);
+////			},
+//			validator: validator.isEmail,
+//			message: '{VALUE} is not a valid email'
+//		}
+//	}, 
+//	password: {
+//		type: String,
+//		required: true,
+//		minlength: 6
+//	},
+//	tokens: [{
+//		access: {
+//			type: String,
+//			required: true
+//		}
+//	}, {
+//		token: {
+//			type: String,
+//			required: true
+//		}
+//	}]
+//	
+//});
+
+//module.exports = {
+//	User: User
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
