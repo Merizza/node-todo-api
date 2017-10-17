e87199b329314cfe4b62459d189c5bcb686fc251
Merizza Khan <merizza.azwir@gmail.com>
2017-10-17 14:15:00 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -176,6 +176,27 @@ app.get('/users/me', authenticate, (req, res) => {
 	
 });
 
+//POST /users/login
+app.post('/users/login', (req, res) => {
+	
+	var body = _.pick(req.body, ['email', 'password']);
+	
+	User.findByCredentials(body.email, body.password).then((user) => {
+		
+		return user.generateAuthToken().then((token) => {
+			
+			res.header('x-auth', token).send(user);
+			
+		});
+		
+	}).catch((e) => {
+		
+		res.status(400).send();
+		
+	});
+	
+});
+
 app.listen(port, () => {
 	
 	console.log(`Started on port ${port}`);
